cc3ac8821fccbca3108a83701a49ecc3c308a784
Maksim <lazmax777@gmail.com>
2023-08-09 12:33:34 +0300

    Initial commit

---
+++ src/test/java/com/broker_manager/BrokerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broker_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
